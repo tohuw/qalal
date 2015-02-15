4b556bf22aa977c280aa292308d81278baeac95e
Ron Scott-Adams <ron@tohuw.net>
2015-02-15 06:17:24 -0500

    Search box now transitions more naturally and clears on defocus.

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,16 @@
-// Main Search Form
+// Search Form Effects
+var searchPlaceholder = $('#tipue_search_input').attr("placeholder")
+
 $('#tipue_search_input')
+    .focus(function() {
+        if (!($(this).attr("placeholder") == searchPlaceholder)) {
+            $(this).attr("placeholder", searchPlaceholder);
+        };
+    })
     .mouseenter(function() {
         $(this).focus();
     })
+    .blur(function() {
+        $(this).val("");
+        $(this).attr("placeholder", "\uf002");
+    });
